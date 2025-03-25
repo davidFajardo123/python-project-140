92476cc49c817905abe988d79a2c6298f3d0dd95
davidFajardo123 <davidmauricio.fajardo@gmail.com>
2025-03-25 18:18:48 -0500

    Juegos mentales Step 3
    Paso 3 (1-2)

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
+# ----------------------- DEP -----------------------------
+from brain_games.cli import welcome_user
+
+# Main -------------------------------------------------------
 def main():
     print("Welcome to the Brain Games!")
-
+    welcome_user()
+    
+# ----------------------------- MAIN ----------------------------|
 if __name__ == "__main__":
     main()
