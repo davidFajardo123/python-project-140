b1a366c18e07c9a9eba0d920a22daf5a8cd4bc9c
davidFajardo123 <davidmauricio.fajardo@gmail.com>
2025-03-25 22:46:52 -0500

    Juegos mentales Step 7
    Pasos 7: (1-4) (-5): Juego Maximo comun divisor MCD o gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,40 @@
+# ------------------------------ DEP -------------------------------------------
+import random
+import math
+from brain_games.cli import welcome_user
+
+# Funcion para generar numeros aleatorios y mcd ----------------------------------------
+def generate_numbers():
+    
+    num1 = random.randint(1, 100)       # Rnd num 1
+    num2 = random.randint(1, 100)       # Rnd num 2
+    gcd_result = math.gcd(num1, num2)   # Calcula el MCD con la funcion gcd
+
+    return num1, num2, gcd_result       # Retorna los numeros y el mcd
+
+# Main ------------------------------------------------------------------------------
+def main():
+    print("Welcome to the Brain Games!")                            # Mensaje inicio
+    name = welcome_user()                                           # Func welcome
+    print("Find the greatest common divisor of given numbers.")     # Mensaje de juego
+
+    correct_answers = 0                                             # Contador preguntas
+
+    while correct_answers < 3:                                      # ciclo preguntas
+        num1, num2, correct_gcd = generate_numbers()                # Llamado a funcion de mcd
+        print(f"Question: {num1} {num2}")                           # Mostrar los numeros random
+        user_answer = input("Your answer: ").strip()                # Esperar respuesta del usuario
+
+        if user_answer == str(correct_gcd):                         # Verificar respuesta
+            print("Correct!")                                       
+            correct_answers += 1                                    # Contador de pregunta correcta
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_gcd}'.")   
+            print(f"Let's try again, {name}!")
+            return                                                  # Termina el juego si hay un error
+
+    print(f"Congratulations, {name}!")                              
+
+# -------------------------- MAIN ---------------------------------------------------------
+if __name__ == "__main__":
+    main()
