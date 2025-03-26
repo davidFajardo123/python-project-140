79d2033a09e574115feb040bb62bee7520aba81c
davidFajardo123 <davidmauricio.fajardo@gmail.com>
2025-03-25 23:11:38 -0500

    Juegos mentales Step 9
    Paso 9 (1-4)(-5): Primos

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,41 @@
+# -------------------------- DEP ---------------------------------
+import random
+from brain_games.cli import welcome_user
+
+# Funcion para saber el numero es primos ----------------------------------------------------------
+def is_prime(n):
+    if n < 2:                               # Mirar si es divisible por 2
+        return False
+    for i in range(2, int(n ** 0.5) + 1):   # Evaluar cantidad de divisores
+        if n % i == 0:                      # Verificar si la division es exacta
+            return False
+    return True
+
+# Main ----------------------------------------------------------------------------------------
+def main():
+    print("Welcome to the Brain Games!")                            # Mensaje de inicio
+    name = welcome_user()                                           # Guarda el nombre del jugador
+    print('Answer "yes" if given number is prime. Otherwise answer "no".') # Mensaje del juego
+
+    correct_answers = 0
+
+    while correct_answers < 3:                                      # Ciclo de preguntas
+        number = random.randint(1, 100)                             # Generar número aleatorio
+        print(f"Question: {number}")
+        user_answer = input("Your answer: ").strip().lower()        # Esperar respuesta
+
+        correct_answer = "yes" if is_prime(number) else "no"        # Sacar respuesta correcta llamando a la funcion
+
+        if user_answer == correct_answer:                           # Comparar respuesta
+            print("Correct!")
+            correct_answers += 1                                    # Contador respuestas correctas
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return                                                  # Termina el juego si hay un error
+
+    print(f"Congratulations, {name}!")
+
+# ------------------- MAIN ------------------------------------------
+if __name__ == "__main__":
+    main()
