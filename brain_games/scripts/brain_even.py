76d16f02871cf76f704419ea6a772f313629d7a3
davidFajardo123 <davidmauricio.fajardo@gmail.com>
2025-03-28 13:08:26 -0500

    Juegos Mentales Rev v6.0
    Cambios en brain-even: correccion de espacios y cambio de lugar los comentarios

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,38 +8,50 @@ def main():
     # import
     import random
     
-    # variables
-    correct_answers = 0                                                     # Contador de respuestas correctas
-    
-    print("Welcome to the Brain Games!")                                    # Welcome message
+    # Contador de respuestas correctas
+    correct_answers = 0                                                     
+    # Welcome message
+    print("Welcome to the Brain Games!")                                    
     name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')     # message only 1 time
+    # message only 1 time
+    print('Answer "yes" if the number is even, otherwise answer "no".')     
 
-    # main loop
-    while correct_answers < 3:                               # Se requieren 3 respuestas correctas seguidas
-        number = random.randint(1, 100)                      # Genera un número aleatorio entre 1 y 100
-        print(f"Question: {number}")                         # Nostrar el numero a verificar 
-        user_answer = input("Your answer: ").strip().lower() # Esperar respuesta del usuario
-        
-        if number%2==0:                                      # Condicional para saber si el numero es par o no
-            answer = "yes"                                   # Si es par respuesta "yes"
+    # Se requieren 3 respuestas correctas seguidas
+    while correct_answers < 3:
+        # Genera un número aleatorio entre 1 y 100                              
+        number = random.randint(1, 100)
+        # Nostrar el numero a verificar                      
+        print(f"Question: {number}")
+        # Esperar respuesta del usuario
+        user_answer = input("Your answer: ").strip().lower()
+        # Condicional para saber si el numero es par o no
+        if number%2==0:
+            # Si es par respuesta "yes"                                   
+            answer = "yes"                                   
         else:
-            answer = "no"                                    # Si es impar respuesta "no"
-            
-        if answer == user_answer:                            # Condicional para saber si estan bien las respuestas
-            correct_answers+=1                               # Se aumenta en 1 el numero de preguntas acertadas 
-            print("Correct!")                                # Se muestra el mensaje de correcto
+            # Si es impar respuesta "no"
+            answer = "no"                                    
+
+        # Condicional para saber si estan bien las respuestas
+        if answer == user_answer:
+            # Se aumenta en 1 el numero de preguntas acertadas                            
+            correct_answers+=1
+            # Se muestra el mensaje de correcto                               
+            print("Correct!")                                
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.") # Devuelve mensaje de mala respuesta
+            # Devuelve mensaje de mala respuesta
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.") 
             print(f"Let's try again, {name}!")
-            #return False                                                                 # Se sale
+            
             exit()
             
         if correct_answers == 3:
             print(f"Congratulations, {name}!")
-            exit()
             
-    #return True                                              # Devuelve "True" si contesta las 3 seguidas y salio del ciclo   
+            exit()
+             
 # ----------------------------- MAIN ----------------------------|
 if __name__ == "__main__":
+    
     main()
+    
