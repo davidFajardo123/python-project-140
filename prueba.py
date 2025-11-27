968423099920c8fe7355b5b3a5b720e0d3a6689c
davidFajardo123 <davidmauricio.fajardo@gmail.com>
2025-11-26 19:57:22 -0500

    prueba commit
    se cambio algo

---
+++ prueba.py
@@ -0,0 +1,20 @@
+numero = 0
+
+while (numero != 5):
+  numero = int(input("Ingresa una opcion: 1) Jugar 2) Opciones 3) Personajes 4) Musica 5) Salir : "))
+  match(numero):
+    case 1:
+        print("....Jugando...")
+    case 2:
+        print("....Opciones...")
+    case 3:
+        print("....Personajes...")
+    case 4:
+        print("....Musica...")
+    case 5:
+        print("....Saliendo...")
+    case _:
+        print("...Opcion no valida...")
+        break
+
+print("Fuera del ciclo")
